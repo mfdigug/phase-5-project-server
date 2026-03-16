566412dfc7a63f68098941bbeac35cfbdab389d3
Michelle Di Guglielmo <mfdigug@gmail.com>
2026-03-16 11:20:09 +1100

    updated post event

--- app.py
+++ app.py
@@ -100,6 +100,53 @@ class Events(Resource):
                   for event in Event.query.all()]
         return make_response(jsonify(events), 200)
 
+    def post(self):
+        data = request.get_json()
+
+        event = Event(
+            title=data.get("title"),
+            date=data.get("date")
+            cuisine_filter=data.get("cuisine_filter"),
+            location_filter=data.get("location_filter"),
+            price_filter=data.get("price_filter"),
+            created_by=data.get("created_by"),
+            selected_restaurant=None
+        )
+
+        db.session.add(event)
+        db.session.commit()
+
+        # set creator automatically to accepted
+        creator_id = data.get("created_by")
+        participants = []
+
+        participants.append(
+            EventParticipant(
+                event=event,
+                user_id=creator_id,
+                rsvp_status="accepted"
+            )
+        )
+
+        # invitees - set automatically to invited
+        invitee_ids = data.get("invitees", [])
+        for user_id in invitee_ids:
+            if user_id != creator_id:
+                participants.append(
+                    EventParticipant(
+                        event=event,
+                        uuser=User.query.get(user_id),
+                        rsvp_status="invited"
+                    )
+                )
+
+        db.session.add_all(participants)
+        db.session.commit()
+
+        return make_response(
+            jsonify(event.to_dict()), 201
+        )
+
 
 api.add_resource(Events, '/api/events')
 
