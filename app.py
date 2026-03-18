eafe1e10ac04067ccb2f2ed098ca8b740bb3e623
Michelle Di Guglielmo <mfdigug@gmail.com>
2026-03-18 15:14:05 +1100

    added cookie work around

--- app.py
+++ app.py
@@ -13,7 +13,12 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['SESSION_COOKIE_SAMESITE'] = "None"
-app.config['SESSION_COOKIE_SECURE'] = True
+
+if os.environ.get("FLASK_ENV") == "development":
+    app.config['SESSION_COOKIE_SECURE'] = False
+else:
+    app.config['SESSION_COOKIE_SECURE'] = True
+
 app.json.compact = False
 
 @app.before_request
