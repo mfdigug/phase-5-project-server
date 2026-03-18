cb463c51f10987feba8f476194b839c043d4f902
Michelle Di Guglielmo <mfdigug@gmail.com>
2026-03-18 14:12:21 +1100

    added resource paths to __init__

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,5 +1,5 @@
 from app import app, api
-from .users import Users, Register
+from .users import Users, Register, Login, Logout
 from .events import Events
 from .event_participants import EventParticipants
 from .restaurants import Restaurants
@@ -7,6 +7,8 @@ from .restaurants import Restaurants
 # Users
 api.add_resource(Users, '/api/users')
 api.add_resource(Register, '/api/register')
+api.add_resource(Login, '/api/login')
+api.add_resource(Logout, '/api/logout')
 
 # Events
 api.add_resource(Events, '/api/events')
