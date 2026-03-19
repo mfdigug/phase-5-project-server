fa15e2e3d3ef4735e9ef187a0a45ed87556aedfe
Michelle Di Guglielmo <mfdigug@gmail.com>
2026-03-20 10:45:58 +1100

    testing new route

--- routes/users.py
+++ routes/users.py
@@ -13,12 +13,15 @@ class Users(Resource):
 
 class UserById(Resource):
     def get(self, id):
-        user = User.query.filter_by(id=id).first()
+        return {"message": "THIS IS NEW CODE"}, 200
+   
+    # def get(self, id):
+    #     user = User.query.filter_by(id=id).first()
         
-        if not user:
-            return make_response({"error": "User not found"}, 404)
+    #     if not user:
+    #         return make_response({"error": "User not found"}, 404)
         
-        return make_response(jsonify(user.to_dict()), 200)
+    #     return make_response(jsonify(user.to_dict()), 200)
 
 
 class CheckSession(Resource):
