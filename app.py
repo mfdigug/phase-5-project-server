e551003bec651560d16b86b74d557a0534bd47ce
Michelle Di Guglielmo <mfdigug@gmail.com>
2026-03-25 17:19:22 +1100

    updated scope

--- app.py
+++ app.py
@@ -15,7 +15,12 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['GOOGLE_OAUTH_CLIENT_ID'] = os.environ.get("GOOGLE_OAUTH_CLIENT_ID") 
 app.config['GOOGLE_OAUTH_CLIENT_SECRET'] = os.environ.get('GOOGLE_OAUTH_CLIENT_SECRET') 
-google_bp = make_google_blueprint(scope=["profile", "email"])
+google_bp = make_google_blueprint(scope=[
+        "openid",
+        "https://www.googleapis.com/auth/userinfo.email",
+        "https://www.googleapis.com/auth/userinfo.profile"
+    ]
+)
 app.register_blueprint(google_bp, url_prefix="/login")
 
 app.config["SESSION_TYPE"] = "filesystem" #remove before deploy to prod
