d2fd6a46ad6ef22571a09b0bb50370fdbb8c2549
Michelle Di Guglielmo <mfdigug@gmail.com>
2026-03-25 21:49:06 +1100

    updated http cookie

--- app.py
+++ app.py
@@ -25,9 +25,10 @@ google_bp = make_google_blueprint(scope=[
 app.register_blueprint(google_bp, url_prefix="/login")
 
 app.config["SESSION_TYPE"] = "filesystem" #remove before deploy to prod
-app.config["SESSION_COOKIE_DOMAIN"] = None #remove before deploy to prod
+app.config["SESSION_COOKIE_DOMAIN"] = ".onrender.com"
 app.config['SESSION_COOKIE_SAMESITE'] = "None" 
 app.config['SESSION_COOKIE_SECURE'] = True
+app.config['SESSION_COOKIE_HTTPONLY'] = True
 
 app.json.compact = False
 
