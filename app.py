1a07c6902aaa9bd9b1d2be7db3fb457dc60db6c3
Michelle Di Guglielmo <mfdigug@gmail.com>
2026-06-26 11:11:06 +1000

    reverted app.py file for deployment

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import os
 
-from flask import Flask
+from flask import Flask, request, session, jsonify, redirect
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from flask_restful import Api
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
-# from flask_dance.contrib.google import make_google_blueprint, google
+from flask_dance.contrib.google import make_google_blueprint, google
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -17,21 +17,21 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-# app.config['GOOGLE_OAUTH_CLIENT_ID'] = os.environ.get("GOOGLE_OAUTH_CLIENT_ID") 
-# app.config['GOOGLE_OAUTH_CLIENT_SECRET'] = os.environ.get('GOOGLE_OAUTH_CLIENT_SECRET') 
-# google_bp = make_google_blueprint(scope=[
-#         "openid",
-#         "https://www.googleapis.com/auth/userinfo.email",
-#         "https://www.googleapis.com/auth/userinfo.profile"
-#     ],
-#     redirect_to="login_success"
-# )
-# app.register_blueprint(google_bp, url_prefix="/login")
+app.config['GOOGLE_OAUTH_CLIENT_ID'] = os.environ.get("GOOGLE_OAUTH_CLIENT_ID") 
+app.config['GOOGLE_OAUTH_CLIENT_SECRET'] = os.environ.get('GOOGLE_OAUTH_CLIENT_SECRET') 
+google_bp = make_google_blueprint(scope=[
+        "openid",
+        "https://www.googleapis.com/auth/userinfo.email",
+        "https://www.googleapis.com/auth/userinfo.profile"
+    ],
+    redirect_to="login_success"
+)
+app.register_blueprint(google_bp, url_prefix="/login")
 
-app.config["SESSION_COOKIE_SECURE"] = False
-app.config["SESSION_COOKIE_SAMESITE"] = "Lax"
-app.config["SESSION_COOKIE_HTTPONLY"] = True
+app.config["SESSION_COOKIE_SECURE"] = True
+app.config["SESSION_COOKIE_SAMESITE"] = "None"
 
+FRONTEND_URL = os.environ.get("FRONTEND_URL", "http://127.0.0.1:5173",  "http://localhost:5173")
 
 app.json.compact = False
 
@@ -48,38 +48,37 @@ bcrypt = Bcrypt(app)
 
 CORS(app, 
     supports_credentials=True, 
-    origins=["http://localhost:5173", "http://127.0.0.1:5173"])
+    origins=["https://reliable-kataifi-750975.netlify.app", "http://localhost:5173", "http://127.0.0.1:5173"])
 
 
 api = Api(app)
 
 
+@app.route("/login/success")
+def login_success():
+    if not google.authorized:
+        return redirect("https://reliable-kataifi-750975.netlify.app/login")
 
-# @app.route("/login/success")
-# def login_success():
-#     if not google.authorized:
-#         return redirect("https://reliable-kataifi-750975.netlify.app/login")
+    resp = google.get("/oauth2/v2/userinfo")
+    user_info = resp.json()
+    email = user_info.get("email")
+    username = email.split("@")[0]
 
-#     resp = google.get("/oauth2/v2/userinfo")
-#     user_info = resp.json()
-#     email = user_info.get("email")
-#     username = email.split("@")[0]
+    from models import User
 
-#     from models import User
+    user = User.query.filter_by(email=email).first()
 
-#     user = User.query.filter_by(email=email).first()
+    if not user:
+        user = User(
+            email=email,
+            username=username
+        )
+        db.session.add(user)
+        db.session.commit()
 
-#     if not user:
-#         user = User(
-#             email=email,
-#             username=username
-#         )
-#         db.session.add(user)
-#         db.session.commit()
+    session["user_id"] = user.id
 
-#     session["user_id"] = user.id
-
-#     return redirect("https://reliable-kataifi-750975.netlify.app/dashboard/restaurants/wishlist")
+    return redirect("https://reliable-kataifi-750975.netlify.app/dashboard/restaurants/wishlist")
 
 
 import routes
